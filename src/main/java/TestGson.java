7e5565d2d20c14c873b224968ddf0ab8de8f2316
Михайлечко Назар <mykhailechko@accordbank.com.ua>
2019-07-12 16:02:57 +0300

    jsonApi

---
+++ src/main/java/TestGson.java
@@ -0,0 +1,22 @@
+import com.google.gson.Gson;
+import model.Example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class TestGson {
+
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://api.privatbank.ua/p24api/pboffice?json&city=%D0%94%D0%BD%D0%B5%D0%BF%D1%80%D0%BE%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D0%BA&address=%D0%A2%D0%B8%D1%82%D0%BE%D0%B2%D0%B0");
+
+        //InputStreamReader reader = new InputStreamReader(url.openStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        Example dto = new Gson().fromJson(reader, Example.class);
+
+        System.out.println(dto);
+
+    }
+
+}
---
+++ src/main/java/model/Example.java
@@ -0,0 +1,120 @@
+package model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Example {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("state")
+    @Expose
+    private String state;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("index")
+    @Expose
+    private String index;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address")
+    @Expose
+    private String address;
+
+    public Example() {}
+
+    public Example(String name, String state, String id, String country, String city, String index, String phone, String email, String address) {
+        super();
+        this.name = name;
+        this.state = state;
+        this.id = id;
+        this.country = country;
+        this.city = city;
+        this.index = index;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getIndex() {
+        return index;
+    }
+    public void setIndex(String index) {
+        this.index = index;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Example{" +
+                "name='" + name + '\'' +
+                ", state='" + state + '\'' +
+                ", id='" + id + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", index='" + index + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
